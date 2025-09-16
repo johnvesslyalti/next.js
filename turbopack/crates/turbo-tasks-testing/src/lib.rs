4ade9472456449b78beafbbc4421eb5642753fd7
Tobias Koppers <tobias.koppers@googlemail.com>
2025-09-16 15:41:44 +0200

    Turbopack: remove lazy invalidated tasks

--- turbopack/crates/turbo-tasks-testing/src/lib.rs
+++ turbopack/crates/turbo-tasks-testing/src/lib.rs
@@ -166,10 +166,6 @@ impl TurboTasksApi for VcStorage {
         // ignore
     }
 
-    fn notify_scheduled_tasks(&self) {
-        // ignore
-    }
-
     fn try_read_task_output(
         &self,
         id: TaskId,
